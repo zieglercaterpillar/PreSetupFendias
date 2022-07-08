018643e1893268d65325fd2acc3009989861f196
zieglercaterpillar <108952081+zieglercaterpillar@users.noreply.github.com>
2022-07-08 15:05:22 -0500

    Switched from Copy-Item to robocopy
    Fixes the issue with the executable having pop ups

--- PreSetupFendias.ps1
+++ PreSetupFendias.ps1
@@ -2,8 +2,8 @@
 ####TL 7/1/2022
 Get-Date > C:\Utilities\PreFendiasLog.txt
 mkdir C:\FendiasDownload >> C:\Utilities\PreFendiasLog.txt
-Copy-Item '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 1.2022' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
-Copy-Item '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 1a.2022' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
-Copy-Item '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 3.2021' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
-Copy-Item '\\e500srv6\Software\Fendias\NewFendiasData\manifests' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
+robocopy '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 1.2022' C:\FendiasDownload\ /e /copyall >> C:\Utilities\PreFendiasLog.txt
+robocopy '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 1a.2022' C:\FendiasDownload\ /e /copyall >> C:\Utilities\PreFendiasLog.txt
+robocopy '\\e500srv6\Software\Fendias\NewFendiasData\Fendias V 3.2021' C:\FendiasDownload\ /e /copyall >> C:\Utilities\PreFendiasLog.txt
+robocopy '\\e500srv6\Software\Fendias\NewFendiasData\manifests' C:\FendiasDownload\ /e /copyall >> C:\Utilities\PreFendiasLog.txt
 exit
