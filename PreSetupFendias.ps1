f369335750eb5972f36c842bf5fec3b82614bb60
zieglercaterpillar <108952081+zieglercaterpillar@users.noreply.github.com>
2022-07-08 14:39:31 -0500

    Added logging
    Added logging. Should fix the bug of pop-up windows coming from the executable

--- PreSetupFendias.ps1
+++ PreSetupFendias.ps1
@@ -1,7 +1,8 @@
 ﻿####This script creates the FendiasDownload directory and copies cached updates to expedite the installation process
 ####TL 7/1/2022
+Get-Date > C:\Utilities\PreFendiasLog.txt
 mkdir C:\FendiasDownload
-Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 1.2022' C:\FendiasDownload -Recurse
-Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 1a.2022' C:\FendiasDownload -Recurse
-Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 3.2021' C:\FendiasDownload -Recurse
+Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 1.2022' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
+Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 1a.2022' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
+Copy-Item '\\e500srv6\Software\Fendias\test\Fendias V 3.2021' C:\FendiasDownload -Recurse >> C:\Utilities\PreFendiasLog.txt
 exit
